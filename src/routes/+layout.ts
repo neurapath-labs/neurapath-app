c7d99ecc3c1d8863c03e5e78b6b7ea31881fe9ac
Martin <Hello@martinjakobsson.se>
2025-08-11 02:42:58 +0200

    Enable page view and page leave tracking in PostHog analytics integration

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -6,8 +6,8 @@ export const load = async () => {
   if (browser) {
     posthog.init(PUBLIC_POSTHOG_KEY, {
       api_host: PUBLIC_POSTHOG_HOST,
-      capture_pageview: false,
-      capture_pageleave: false,
+      capture_pageview: true,
+      capture_pageleave: true,
       capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
     });
   }
